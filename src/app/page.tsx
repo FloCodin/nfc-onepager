62b5497b020e25d854212c7258c5a5ecd8cd002d
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-04-29 11:15:55 +0200

    Add tabs component and extend global styles
    Introduced a Tabs component using Radix UI, alongside updates to typography and styles in `globals.css`. Updated dependencies and configurations for TailwindCSS and added utility functions for class merging.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,117 @@
+"use client";
+
 import Image from "next/image";
+import {Tabs, TabsContent, TabsList, TabsTrigger,} from "@/components/ui/tabs";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+            <div className="bg-white shadow-xl rounded-2xl max-w-md w-full p-6 text-center space-y-6">
+                <div className="flex flex-col items-center">
+                  <Link href="https://www.codin.ch">
+                    <Image
+                        src="/codin%20black@0.75x.webp"
+                        alt="codin logo"
+                        className="h-12 mb-4"
+                        height={100}
+                        width={100}
+                    />
+                  </Link>
+                    <Link href="https://www.codin.ch">
+                    <Image
+                        src="/img.png"
+                        alt="Florian Rogenmoser"
+                        className="w-36 h-36 rounded-full border-4 border-white shadow-md"
+                        height={256}
+                        width={256}
+                    />
+                  </Link>
+                    <h2 className="text-xl font-semibold mt-2 text-emerald-400">
+                        Florian Rogenmoser
+                    </h2>
+                    <p className="text-gray-500">Applikationsentwickler</p>
+                </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+                <Tabs defaultValue="business" className="w-full mt-4">
+                    <TabsList className="grid w-full grid-cols-2 bg-gray-200">
+                        <TabsTrigger value="business">Business</TabsTrigger>
+                        <TabsTrigger value="private">Private</TabsTrigger>
+                    </TabsList>
+
+                    <TabsContent value="business">
+                        <div className="space-y-3 mt-4">
+                            <Link
+                                href="/vcard.vcf"
+                                download
+                                className="block bg-black text-emerald-400 rounded-full py-2 font-semibold"
+                            >
+                                Herunterladen
+                            </Link>
+                            <Link
+                                href="tel:+41629232222"
+                                className="block bg-black text-emerald-400 rounded-full py-2 font-semibold"
+                            >
+                                Anrufen
+                            </Link>
+                            <Link
+                                href="mailto:florian.rogenmoser@codin.ch"
+                                className="block bg-black text-emerald-400 rounded-full py-2 font-semibold"
+                            >
+                                E-Mail schreiben
+                            </Link>
+
+                            <Link
+                                href="//github.com/FloCodin"
+                                target="_blank"
+                                className="block bg-black text-emerald-400 rounded-full py-2 font-semibold"
+                            >
+                                GitHub
+                            </Link>
+
+                        </div>
+
+                        <div className="text-sm text-left mt-6 text-emerald-400 ">
+                            <p className="font-semibold">Kontaktdaten</p>
+                            <p>+41 62 923 22 22</p>
+                            <p>hello@codin.ch</p>
+                            <p>https://www.codin.ch</p>
+                            <br/>
+                            <p className="font-semibold">Adresse</p>
+                            <p>codin GmbH</p>
+                            <p>Hauptstrasse 64</p>
+                            <p>5070 Frick</p>
+                            <p>Schweiz</p>
+                        </div>
+                    </TabsContent>
+
+                    <TabsContent value="private">
+                        <div className="mt-4 space-y-3">
+                            <Link
+                                href="https://www.instagram.com/flothedeveloper/"
+                                target="_blank"
+                                className="block bg-black text-emerald-400 rounded-full py-2 font-semibold"
+                            >
+                                Instagram
+                            </Link>
+                            <Link
+                                href="https://wa.me/41795321727"
+                                target="_blank"
+                                className="block bg-black text-emerald-400 rounded-full py-2 font-semibold"
+                            >
+                                WhatsApp schreiben
+                            </Link>
+                            <Link
+                                href=""
+                                target="_blank"
+                                className="block bg-black text-emerald-400 rounded-full py-2 font-semibold"
+                            >
+                                Portfolio Website
+                            </Link>
+                        </div>
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </main>
+    );
 }
